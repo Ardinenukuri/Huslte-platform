713f6a5628a791bc5df9044ccb476a7f1d877f22
Ardinenukuri <109416235+Ardinenukuri@users.noreply.github.com>
2025-02-14 23:29:03 +0200

    okay

--- Hustle/capstone/celery.py
+++ Hustle/capstone/celery.py
@@ -1,15 +1,12 @@
 import os
 from celery import Celery
 
-# Set default Django settings for Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hustle.settings')
 
 app = Celery('capstone')
 
-# Load task modules from all registered Django apps
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Auto-discover tasks in Django apps
 app.autodiscover_tasks()
 
 @app.task(bind=True)
