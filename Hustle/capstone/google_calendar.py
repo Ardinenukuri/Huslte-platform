40d306b3115a81875d88848e4879f04f6e2e01b1
Ardinenukuri <109416235+Ardinenukuri@users.noreply.github.com>
2025-02-14 23:27:52 +0200

    okay

--- Hustle/capstone/google_calendar.py
+++ Hustle/capstone/google_calendar.py
@@ -4,18 +4,14 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
-# Path to the credentials file
 CREDENTIALS_FILE = os.path.join(os.path.dirname(__file__), '..', 'credentials.json')
 
-# If modifying these SCOPES, delete the token.json file.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 def get_google_calendar_service():
     creds = None
-    # The file token.json stores the user's access and refresh tokens.
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -24,7 +20,6 @@ def get_google_calendar_service():
                 CREDENTIALS_FILE, SCOPES, redirect_uri='http://localhost:8000/auth/google/callback/'
             )
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
     return build('calendar', 'v3', credentials=creds)
