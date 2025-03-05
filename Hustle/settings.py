5f7955a1c8865c7d53d6c5448e580652eb87d5ab
iAmNsengi <iamnsengi@icloud.com>
2025-03-05 22:02:30 +0200

    fx: media url on deployment

--- Hustle/settings.py
+++ Hustle/settings.py
@@ -86,6 +86,9 @@ STATICFILES_DIRS = [
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+MEDIA_ROOT = os.path .join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
+
 # # Media files configuration
 # if not DEBUG:
 #     MEDIA_ROOT = '/media'
