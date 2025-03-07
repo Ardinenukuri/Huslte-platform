e50dd795889d76f301c62b7017497c37508eb897
Ardinenukuri <a.nukuri@alustudent.com>
2025-03-07 14:31:57 +0200

    Removed unwanted directories from the repository

--- capstone/Hustle/capstone/templatetags/custom_filters.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def get_item(dictionary, key):
-    """Custom template filter to access dictionary keys safely"""
-    return dictionary.get(key, None) if dictionary else None
